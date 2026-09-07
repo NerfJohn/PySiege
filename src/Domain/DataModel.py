880f609538b709aadc2dcf97fe9719c1d8ab6837
John Eslinger <johneslingertx@gmail.com>
2026-09-06 21:26:05 -0500

    one file example of keyboard input for control vs command.

--- src/Domain/DataModel.py
+++ src/Domain/DataModel.py
@@ -2,14 +2,38 @@
 # DataModel.py: central/shared of the entire program.
 ################################################################################
 
+from pygame.event import Event
+
 ################################################################################
 # Model of central data.
 class DataModel():
     # Identity.
-    m_prgmName = "" # str: name of the program
+    m_prgmName   = ""    # str: name of the program
 
-    # Funcs.
+    # State Machine.
+    m_gettingCmd = False # bool: direct keyboard input to command vs controls.
+
+    # Keyboard input.
+    m_keyEvt     = None  # Event: latest key-press event
+
+    # Command input.
+    m_cmdBuffer  = ""    # str: command being created/executed
+    m_cmdRdy     = False # bool: command creation vs ready to execute
+
+    # Defaults.
     def __init__(self):
-        self.m_prgmName = "PySiege.py"
+        self.m_prgmName   = "PySiege.py"
+        self.m_gettingCmd = False
+        self.m_keyEvt     = None
+        self.m_cmdBuffer  = ""
+        self.m_cmdRdy     = False
     def __str__(self):
-        return f"(prgmName = {self.m_prgmName})"
+        return f"(prgmName = {self.m_prgmName}, "     \
+                f"gettingCmd = {self.m_gettingCmd}, " \
+                f"keyEvt = {self.m_keyEvt}, "         \
+                f"cmdBuffer = {self.m_cmdBuffer}, "   \
+                f"cmdRdy = {self.m_cmdRdy})"
+
+    # Converts.
+    def keyEvt(self) -> Event:
+        return self.m_keyEvt
--- src/main.py
+++ src/main.py
@@ -2,10 +2,69 @@
 # main.py: root of the program- setup and loop functions.
 ################################################################################
 
+import pygame
+from pygame.locals import *
+
 from Domain.DataModel import DataModel
+
+################################################################################
+
+# FPS of program.
+MAIN_FPS = 60
 
 ################################################################################
 # Start of program.
 if __name__ == "__main__":
+    # Init central data.
     model = DataModel()
-    print(model)
+
+    # Init pygame details.
+    pygame.init()
+    screen = pygame.display.set_mode((300, 300))
+    pygame.display.set_caption(model.m_prgmName)
+    clock = pygame.time.Clock()
+
+    # Main loop.
+    running = True
+    while running:
+        # INPUT: Get keyboard input.
+        for event in pygame.event.get():
+            # Exit scenarios.
+            if (event.type == QUIT) or ((event.type == KEYDOWN) and (event.key == K_ESCAPE)):
+                running = False
+
+            # Key presses (grab first for processing).
+            if (event.type == KEYDOWN) and (model.keyEvt() == None):
+                model.m_keyEvt = event
+
+        # PROCESS: keyboard inputs.
+        keyEvt = model.keyEvt()
+        if keyEvt != None:
+            # Keyboard vs command.
+            if model.m_gettingCmd:
+                if keyEvt.key == K_RETURN:
+                    model.m_cmdRdy = True
+                elif keyEvt.key == K_BACKSPACE:
+                    model.m_cmdBuffer = model.m_cmdBuffer[:-1]
+                elif keyEvt.unicode and keyEvt.unicode.isprintable():
+                    model.m_cmdBuffer += keyEvt.unicode
+            else:
+                if keyEvt.key == K_SLASH:
+                    print("Intake command...")
+                    model.m_gettingCmd = True
+                else:
+                    print(f"Key pressed: {pygame.key.name(keyEvt.key)}")  # Prints the key name
+            model.m_keyEvt = None
+
+        # OUTPUT: run command.
+        if model.m_cmdRdy:
+            print(f"Run Command: {model.m_cmdBuffer}")
+            model.m_cmdBuffer  = ""
+            model.m_cmdRdy     = False
+            model.m_gettingCmd = False
+
+        # MAINTENANCE: Maintain framerate.
+        clock.tick(MAIN_FPS)
+
+    # Shutdown program.
+    pygame.quit()
